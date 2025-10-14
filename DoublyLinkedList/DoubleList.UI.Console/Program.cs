bbf8db7912d9139d6f6767bf1e386e53db95efe0
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-10-14 18:12:55 -0500

    Practice insert begining and end!

---
+++ DoublyLinkedList/DoubleList.UI.Console/Program.cs
@@ -0,0 +1,73 @@
+﻿using DoubleList;
+
+var list = new DoublyLinkedList<string>();
+var opc = "0";
+
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "0":
+            Console.WriteLine("¡Regresa para otra lista con Taran!");
+            break;
+        case "1":
+            Console.WriteLine("¿donde quieres adicionar el nuevo dato? ");
+            Console.WriteLine("a. Al inicio.");
+            Console.WriteLine("b. Al final.");
+            Console.Write("Elige una opción.");
+            var subopc = Console.ReadLine();
+
+            Console.Write("Ingresa el dato a adicionar: ");
+            var data = Console.ReadLine();
+
+            if (data != null)
+            {
+                if (subopc == "a" || subopc == "A")
+                {
+                    list.InsertAtBeginning(data);
+                    Console.WriteLine("Dato adicionado al inicio.");
+                }
+                else if (subopc == "b" || subopc == "B")
+                {
+                    list.InsertAtEnd(data);
+                    Console.WriteLine("Dato adicionado al inicio.");
+                }
+                else
+                {
+                    Console.WriteLine("Opción invlida");
+                }
+            }
+            break;
+        case "2":
+            Console.WriteLine(list.GetForward());
+            break;
+        case "3":
+            Console.WriteLine(list.GetBackward());
+            break;
+        case "4":
+            Console.WriteLine("Enter the data  to remove: ");
+            var dataRemove = Console.ReadLine();
+            if (dataRemove != null)
+            {
+                list.Remove(dataRemove);
+                Console.WriteLine("Item removed");
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+} while (opc != "0");
+string Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine("1. Adicionar dato.");
+    Console.WriteLine("2. Mostrar hacia adelante.");
+    Console.WriteLine("3. Mostarr hacia atras.");
+    Console.WriteLine("4. Show list backward.");
+    Console.WriteLine("5. Remove the list.");
+    Console.WriteLine("0. Exit.");
+    Console.WriteLine("ENTER YOUR OPTION");
+    return Console.ReadLine()!;
+}
---
+++ DoublyLinkedList/DoubleList/DoublyLinkedList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleList;
+
+ public class DoublyLinkedList<T>
+{
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+
+    public DoublyLinkedList()
+    {
+        _head = null;
+        _tail = null;
+    }
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+    }
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_tail == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+    }
+    public string GetForward()
+    {
+        var output = string.Empty;
+        var current = _head;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Next;
+        }
+        if (output.Length >= 5)
+            output = output.Remove(output.Length - 5);
+        return output;
+    }
+    public string GetBackward()
+    {
+        var output = string.Empty;
+        var current = _tail;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Prev;
+        }
+        if (output.Length >= 5)
+            output = output.Remove(output.Length - 5);
+        return output;
+    }
+    public void Remove(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else
+                {
+                    _head = current.Next;
+                }
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else
+                {
+                    _tail = current.Prev;
+                }
+                break;
+            }
+            current = current.Next;
+        }
+    }
+}
+
+
+
+
