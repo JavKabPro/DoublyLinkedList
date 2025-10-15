dcbf1bab2049c4dc41d4aa6a4ef506ce3c7cfd74
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-10-14 19:30:46 -0500

    I do it the point one, two, three and four the workshop... let's go the exit and continue!

--- DoublyLinkedList/DoubleList.UI.Console/Program.cs
+++ DoublyLinkedList/DoubleList.UI.Console/Program.cs
@@ -12,32 +12,8 @@ do
             Console.WriteLine("¡Regresa para otra lista con Taran!");
             break;
         case "1":
-            Console.WriteLine("¿donde quieres adicionar el nuevo dato? ");
-            Console.WriteLine("a. Al inicio.");
-            Console.WriteLine("b. Al final.");
-            Console.Write("Elige una opción.");
-            var subopc = Console.ReadLine();
-
             Console.Write("Ingresa el dato a adicionar: ");
-            var data = Console.ReadLine();
-
-            if (data != null)
-            {
-                if (subopc == "a" || subopc == "A")
-                {
-                    list.InsertAtBeginning(data);
-                    Console.WriteLine("Dato adicionado al inicio.");
-                }
-                else if (subopc == "b" || subopc == "B")
-                {
-                    list.InsertAtEnd(data);
-                    Console.WriteLine("Dato adicionado al inicio.");
-                }
-                else
-                {
-                    Console.WriteLine("Opción invlida");
-                }
-            }
+            list.InsertOrdered(Console.ReadLine()!);
             break;
         case "2":
             Console.WriteLine(list.GetForward());
@@ -46,6 +22,11 @@ do
             Console.WriteLine(list.GetBackward());
             break;
         case "4":
+            list.Reverse();
+            Console.WriteLine("La lista ha sido invertida (forma descendente).");
+            Console.WriteLine(list.GetForward());
+            break;
+        case "5":
             Console.WriteLine("Enter the data  to remove: ");
             var dataRemove = Console.ReadLine();
             if (dataRemove != null)
@@ -55,7 +36,7 @@ do
             }
             break;
         default:
-            Console.WriteLine("Invalid option");
+            Console.WriteLine("Opción inválida");
             break;
     }
 } while (opc != "0");
@@ -65,8 +46,7 @@ string Menu()
     Console.WriteLine("1. Adicionar dato.");
     Console.WriteLine("2. Mostrar hacia adelante.");
     Console.WriteLine("3. Mostarr hacia atras.");
-    Console.WriteLine("4. Remover de la lista.");
-    Console.WriteLine("5. Remove the list.");
+    Console.WriteLine("4. Ordenar descendentemente");
     Console.WriteLine("0. Exit.");
     Console.WriteLine("ENTER YOUR OPTION");
     return Console.ReadLine()!;
--- DoublyLinkedList/DoubleList/DoublyLinkedList.cs
+++ DoublyLinkedList/DoubleList/DoublyLinkedList.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DoubleList;
 
@@ -45,6 +47,78 @@ namespace DoubleList;
             newNode.Prev = _tail;
             _tail = newNode;
         }
+    }
+    public void InsertOrdered(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+
+        else if (Comparer<T>.Default.Compare(data, _head.Data) < 0)
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+            return;
+        }
+        else
+        {
+            var current = _head;
+            while (current != null)
+            {
+                int comparison = Comparer<T>.Default.Compare(data, current.Data);
+                if (comparison < 0)
+                {
+                    newNode.Next = current;
+                    newNode.Prev = current.Prev;
+                    if (current.Prev != null)
+                        current.Prev.Next = newNode;
+                    else
+                        _head = newNode;
+                    current.Prev = newNode;
+                    return;
+                }
+                else if (comparison == 0)
+                {
+                    while (current.Next != null && Comparer<T>.Default.Compare(data, current.Next.Data) == 0)
+                    {
+                        current = current.Next;
+                    }
+                    newNode.Next = current.Next;
+                    newNode.Prev = current;
+                    if (current.Next != null)
+                        current.Next.Prev = newNode;
+                    else
+                        _tail = newNode;
+                    current.Next = newNode;
+                    return;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+            _tail!.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+    }
+    public void Reverse()
+    {
+        var current = _head;
+        while (current != null)
+        {
+            var temp = current.Next;
+            current.Next = current.Prev;
+            current.Prev = temp;
+            current = temp;
+        }
+        var auxHead = _head;
+        _head = _tail;
+        _tail = auxHead;
     }
     public string GetForward()
     {
